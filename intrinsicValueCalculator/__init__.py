f3c216fd5149e17357519b66e11fd953e05a8501
Sean Paterson <seanwpaterson@gmail.com>
2021-07-16 13:07:09 +0100

    Fixed errors from tutorial

--- intrinsicValueCalculator/__init__.py
+++ intrinsicValueCalculator/__init__.py
@@ -1,16 +1,18 @@
 import os
+
 from flask import Flask
 
+
 def create_app(test_config=None):
-    # create abd configure the app
+    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'intrinsicValueCalculatorsqlite'),
+        DATABASE=os.path.join(app.instance_path, 'intrinsicValueCalculator.sqlite'),
     )
 
     if test_config is None:
-        # load the instance config, of it exists, when not testing
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
@@ -20,11 +22,11 @@ def create_app(test_config=None):
     try:
         os.makedirs(app.instance_path)
     except OSError:
-        passed
+        pass
 
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return "Hello, World!"
+        return 'Hello, World!'
 
     return app
